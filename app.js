44311f0285c034393008d20044d88815902cd02b
Craig Rodrigues <PagingCraig@gmail.com>
2017-01-18 21:21:23 -0500

    Add structure to HTML and add event listener to refresh button

--- app.js
+++ app.js
@@ -2,18 +2,35 @@ $(document).ready(function(){
   var $body = $('body');
   //$body.html('');
 
-  // Need to create random tweets and show it to the user.
-  // scheduleNextTweet();
-
   // Generates a list of 10 random tweets based on the streams.home object length.
-  var index = streams.home.length - 1;
-  while(index >= 0){
-    var tweet = streams.home[index];
-    var $tweet = $('<div></div>');
-    // Put the info from the object into the div
-    $tweet.text('@' + tweet.user + ': ' + tweet.message);
-    // Append that div to the body
-    $tweet.appendTo($body);
-    index -= 1;
+  // var index = streams.home.length - 1;
+  // while(index >= 0){
+  //   var tweet = streams.home[index];
+  //   var $tweet = $('<div></div>');
+  //   // Put the info from the object into the div
+  //   $tweet.text('@' + tweet.user + ': ' + tweet.message);
+  //   // Append that div to the body
+  //   $tweet.appendTo($body);
+  //   index -= 1;
+  // }
+
+  // Show the user new tweets somehow. (You can show them automatically as they're created, or create a button that displays new tweets.)
+  const refreshStream = function () {
+    var index = streams.home.length - 1;
+    while(index >= 0) {
+      var tweet = streams.home[index];
+      var $tweet = $('<li></li>');
+      // Put the info from the object into the div
+      $tweet.text('@' + tweet.user + ': ' + tweet.message);
+      // Append that div to the body
+      $tweet.appendTo($body);
+      index -= 1;
+    }
   }
+  // Initial tweets
+  refreshStream();
+
+  // Refresh stream every time the button is clicked
+  $body.on('click', 'button', refreshStream);
+
 });
