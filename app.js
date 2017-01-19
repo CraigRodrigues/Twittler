37d5f27c04ff7483bf2e727ba2c7734849bd5e43
Craig Rodrigues <PagingCraig@gmail.com>
2017-01-18 19:08:47 -0500

    Move all javascript from index.html to app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+  var $body = $('body');
+  $body.html('');
+
+  // Need to create random tweets and show it to the user.
+  // scheduleNextTweet();
+
+  // Generates a list of 10 random tweets based on the streams.home object length.
+  var index = streams.home.length - 1;
+  while(index >= 0){
+    var tweet = streams.home[index];
+    var $tweet = $('<div></div>');
+    // Put the info from the object into the div
+    $tweet.text('@' + tweet.user + ': ' + tweet.message);
+    // Append that div to the body
+    $tweet.appendTo($body);
+    index -= 1;
+  }
+
+  var $refreshButton = $('<button>Refresh</button>');
+  $refreshButton.appendTo($body);
+
+});
